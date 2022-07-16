e7b050ae8c5fbfe4f43c428c5d3f9e1dd25045e2
Arkadii Kozhuhow <arkadii_kozhuhow@mail.ru>
2022-07-16 17:11:15 +0700

    add HTML-forms -hw

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -2,7 +2,7 @@ const chkInterestsActive = document.querySelectorAll('.interests.interests_activ
 const chkInterests = document.querySelectorAll('input');
 
 chkInterests[0].addEventListener('change', (e) => {
-	if ((e.target.parentNode.parentNode.classList == 'interest') && (chkInterests[1].checked == false && chkInterests[2].checked == false)) {
+	if ((chkInterests[1].checked == false || chkInterests[2].checked == false)) {
 		chkInterests[1].checked = true
 		chkInterests[2].checked = true 
 	} else {
@@ -12,7 +12,7 @@ chkInterests[0].addEventListener('change', (e) => {
 });
 
 chkInterests[3].addEventListener('change', (e) => {
-	if ((e.target.parentNode.parentNode.classList == 'interest') && (chkInterests[4].checked == false && chkInterests[5].checked == false)) {
+	if ((e.target.parentNode.parentNode.classList == 'interest') && (chkInterests[4].checked == false || chkInterests[5].checked == false)) {
 		chkInterests[4].checked = true
 		chkInterests[5].checked = true 
 	} else {
